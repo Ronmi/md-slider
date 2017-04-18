f45b4b81326b9091f1dee62d7e6636ce9b5a28eb
Ronmi Ren <ronmi.ren@gmail.com>
2017-04-18 14:54:04 +0800

    加上 footnote 功能

--- html.go
+++ html.go
@@ -34,7 +34,30 @@ type MDText string
 
 // Render for text, expands markdown formats
 func (t MDText) Render() string {
-	return string(blackfriday.MarkdownCommon([]byte(t)))
+	r := blackfriday.HtmlRenderer(
+		0|
+			blackfriday.HTML_USE_XHTML|
+			blackfriday.HTML_USE_SMARTYPANTS|
+			blackfriday.HTML_SMARTYPANTS_FRACTIONS|
+			blackfriday.HTML_SMARTYPANTS_DASHES|
+			blackfriday.HTML_SMARTYPANTS_LATEX_DASHES,
+		"", "",
+	)
+	return string(blackfriday.Markdown(
+		[]byte(t),
+		r,
+		0|
+			blackfriday.EXTENSION_NO_INTRA_EMPHASIS|
+			blackfriday.EXTENSION_TABLES|
+			blackfriday.EXTENSION_FENCED_CODE|
+			blackfriday.EXTENSION_AUTOLINK|
+			blackfriday.EXTENSION_STRIKETHROUGH|
+			blackfriday.EXTENSION_SPACE_HEADERS|
+			blackfriday.EXTENSION_HEADER_IDS|
+			blackfriday.EXTENSION_BACKSLASH_LINE_BREAK|
+			blackfriday.EXTENSION_DEFINITION_LISTS|
+			blackfriday.EXTENSION_FOOTNOTES,
+	))
 }
 
 // Prop represents an HTML property
